f5cb4081a91a735767410095eccded87fa99607a
marfushhaaa <p1m455w96@gmail.com>
2024-01-07 15:28:12 +0100

    feat: connect to database
    This connected code with Mongodatabase

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
 dotenv.config();
 
+const client = new MongoClient(process.env.MONGO_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+//mongoose.connect(process.env.MONGO_URL);
+//console.log("connected to mongo");
+
+
 //to use app, our realtime server
 app.listen(7770, ()=>{
-  console.log("backend is readywrtwrgt")
-})
+  console.log("backend is readywrtwrgt");
+});
