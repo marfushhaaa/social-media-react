072065e51b723b3827f96e4eaa4202d3014963c5
marfushhaaa <p1m455w96@gmail.com>
2024-01-13 18:46:10 +0100

    feat: unfollow a User
    This unfollows the user and delets id from his follower's array

--- routes/users.js
+++ routes/users.js
@@ -71,5 +71,25 @@ router.put("/:id/follow", async (req, res)=>{
     res.status(403).json("you cant follow yourself!!!")
   }
 })
+
 //unfollow a user
+router.put("/:id/unfollow", async (req, res)=>{
+  if(req.body.userId !== req.params.id){
+    try{
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if(user.followers.includes(req.body.userId)){
+        await user.updateOne({$pull:{followers:req.body.userId}});
+        await currentUser.updateOne({$pull:{followings:req.params.id}});
+        res.status(200).json("user has been unfollowed");
+      }else {
+        res.status(403).json("you wanna follow this user?")
+      }
+    }catch(error){
+      res.status(500).json(error);
+    }
+  }else{
+    res.status(403).json("you cant unfollow yourself!!!")
+  }
+})
 module.exports = router;
